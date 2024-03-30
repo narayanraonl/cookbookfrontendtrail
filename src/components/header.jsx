5bb1b9f211118acf81c08610596a828f73170c3a
narayanraonl <62981865+narayanraonl@users.noreply.github.com>
2024-03-30 12:04:49 +0530

    Add files via upload

---
+++ src/components/header.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from "react";
+import {Navigate} from "react-router-dom";
+import { useUserContext } from "./userContex";
+
+const useurl="http://localhost:4000"
+export default function Header(){
+    const userContext = useUserContext();
+    useEffect(() => {
+        fetch(useurl+'/authorized', {
+                credentials:'include', 
+        }).then(response =>{
+                response.json().then(userInfo => {
+                    userContext.login(userInfo);
+                })
+            })
+    },[]);
+    
+    console.log(userContext.user);
+    function logout(){
+        fetch(useurl+'/logout',{
+            credentials:'include',
+            method:'POST',
+        }).then(() => {
+            userContext.logout();
+        })
+    }
+    
+    const username = userContext.user;
+    return(
+        <div>
+        <nav>
+            <ul class="nav1">
+                <li>CookIt</li>
+            </ul>
+            <ul class="nav2">
+                <a href="/"><li>Home</li></a>
+                <a href="/recipe"><li>Recipes</li></a>
+                <a href="/posts"><li>Posts</li></a>
+                {username && (
+                    <>
+                    <a href="/yourrecipe"><li>Your Recipes</li></a>
+                    <a onClick={logout}>Logout</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                    <a href="/signin"><li>Signin</li></a>
+                    </>
+                )}
+            </ul>
+        </nav>
+    </div>
+    )
+}
---
+++ src/components/posts.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Post from "./post";
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  const useurl="http://localhost:4000"
+  useEffect(() => {
+    fetch(useurl+"/posts").then((response) => {
+      response.json().then((posts) => {
+        console.log(posts);
+        setPosts(posts);
+      });
+    });
+  }, []);
+
+  return (
+    <div class="postbg">
+      <div className="post">
+        <div></div>
+        <div className="post-con">
+          {posts.length > 0 && posts.map((post) => <Post {...post} />)}
+        </div>
+      </div>
+    </div>
+  );
+}
